eddd01b9b084bb3b4e6c00b458f44a5300e322ba
farjanarahman <mahfuzurrahman.dev@gmail.com>
2023-07-24 22:35:57 +0600

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,6 +33,12 @@ async function run() {
       const cursor = collegeCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    })
+
+    app.get('colleges/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await collegeCollection.findOne(query);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
