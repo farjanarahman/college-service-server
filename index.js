d4778c10b87c5634080cc3daa4ae18917f85a583
farjanarahman <mahfuzurrahman.dev@gmail.com>
2023-07-28 21:39:10 +0600

    college details

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const collegeCollection = client.db('collegeDB').collection('colleges');
 
@@ -39,6 +39,7 @@ async function run() {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await collegeCollection.findOne(query);
+      res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
